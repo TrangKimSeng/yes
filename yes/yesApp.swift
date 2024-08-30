25d4915b8376718e76d346e31a8b0e84d66ceca2
Trang Kim-Seng <202267895@collegeahuntsic.qc.ca>
2024-08-30 11:46:12 -0400

    Initial Commit

---
+++ yes/yesApp.swift
@@ -0,0 +1,17 @@
+//
+//  yesApp.swift
+//  yes
+//
+//  Created by Kim-Seng Trang (Étudiant) on 2024-08-30.
+//
+
+import SwiftUI
+
+@main
+struct yesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
