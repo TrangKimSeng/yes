ef803a03a01468f679bdda53430f678eb9d0c721
Trang Kim-Seng <202267895@collegeahuntsic.qc.ca>
2024-08-30 12:43:18 -0400

    change welcome message to "lkajflkdsjfldkfjdlf"

--- yes/ContentView.swift
+++ yes/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("")
+            Text("al;skdf;lajsdf")
         }
         .padding()
     }
